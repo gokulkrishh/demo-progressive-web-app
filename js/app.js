1b1adf2396e4e9f5478c4f5126b1826882953101
gokulkrishh <krishnangokul9@gmail.com>
2016-01-03 13:02:02 +0530

    Imitated application shell type achitecture

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ $(function () {
 
   $.get(url)
   .success(function (response) {
+    console.log(response)
     response.map(function (repo, index) {
       return(
         $('#main').append(
           "<div class=\"container\">" +
-          "<p> <span>Name:</span> <a href=''>" + repo.name + "</a></p>" +
+          "<p> <span>Name:</span> <a href='"+ repo.html_url + "'>" + repo.name + "</a></p>" +
           "<p> <span>Star:</span> " + repo.stargazers_count + "</p>" +
           "<p> <span>Fork:</span> " + repo.forks_count + "</p>" +
           "</div>"
@@ -17,5 +18,22 @@ $(function () {
   })
   .error(function (error) {
     console.log(error);
+  });
+
+
+  $('#menu-overlay, .menu-icon, #menu a').on('click', function (event) {
+    event.stopPropagation();
+
+    var $menuEle = $('#menu');
+
+    if ($menuEle.hasClass('visible')) {
+      $menuEle.removeClass('visible');
+      $('#menu-overlay').removeClass('visible');
+    }
+    else {
+      $menuEle.addClass('visible');
+      $('#menu-overlay').addClass('visible');
+    }
+
   });
 });
