453f7ad57a2b2dc1336606a1fa91199f39ebed5f
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-02-26 07:10:15 +0530

    Updated toast msg style

--- js/toast.js
+++ js/toast.js
@@ -1,30 +1,19 @@
 (function (exports) {
   'use strict';
 
-  var toastContainer = document.querySelector('.toast__container');
- 
-  //To show notification
   function toast(msg, options) {
     if (!msg) return;
-
     options = options || 3000;
-
-    var toastMsg = document.createElement('div');
     
-    toastMsg.className = 'toast__msg';
+    var toastMsg = document.querySelector('.toast__msg');
     toastMsg.textContent = msg;
-
-    toastContainer.appendChild(toastMsg);
-
+    toastMsg.classList.add("toast__msg--show");
+    
     //Show toast for 3secs and hide it
     setTimeout(function () {
-      toastMsg.classList.add('toast__msg--hide');
+      toastMsg.classList.remove("toast__msg--show");
+      toastMsg.textContent = "";
     }, options);
-
-    //Remove the element after hiding
-    toastMsg.addEventListener('transitionend', function (event) {
-      event.target.parentNode.removeChild(event.target);
-    });
   }
 
   exports.toast = toast; //Make this method available in global
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 //Cache polyfil to support cacheAPI in all browsers
 importScripts('./cache-polyfill.js');
 
-var cacheName = 'cache-v3';
+var cacheName = 'cache-v2';
 
 //Files to save in cache
 var files = [
