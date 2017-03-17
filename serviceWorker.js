c57941f1840662bd70918fd487484c2f5a9b413b
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-03-17 20:03:07 +0530

    Updated origin trail

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 //Cache polyfil to support cacheAPI in all browsers
 importScripts('./cache-polyfill.js');
 
-var cacheName = 'cache-v2';
+var cacheName = 'cache-v3';
 
 //Files to save in cache
 var files = [
